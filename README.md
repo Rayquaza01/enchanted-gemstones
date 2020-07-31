7a0cc39ed5e8c064cdb638c9b349d13c3b659176
Joe Jarvis <joe.jarvis109@gmail.com>
2020-07-31 19:34:13 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# enchanted-gemstones
+A Pico-8 Magic Jewlery/Columns Clone (Submission for [LOWREZJAM 2020](https://itch.io/jam/lowrezjam-2020))
---
+++ bin/export.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pico8 enchanted-gemstones.p8 -export docs/index.html
+pico8 enchanted-gemstones.p8 -export enchanted-gemstones.bin
