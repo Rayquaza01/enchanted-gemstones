1fd89128eda9c6616c844471a3cfc81846ce8e12
Joe Jarvis <joe.jarvis109@gmail.com>
2020-08-02 16:28:42 -0400

    Fix co-ords on game screen

--- enchanted-gemstones/3.p8.lua
+++ enchanted-gemstones/3.p8.lua
@@ -4,7 +4,13 @@
 function reset_game()
 	game = {}
 	game.width = 6
-	game.height = 15
+	game.height = 13
+
+	game.x = 20
+	game.y = 8
+
+	game.nextx = 52
+	game.nexty = 15
 
 	game.startx = 4
 	game.starty = 3
@@ -32,14 +38,14 @@ function update_game()
 end
 
 function draw_game()
-	print("★", 1, 0, 10)
-	print(left_pad(tostr(scores[game.mode + 1][1].score), 5, "0"), 9, 0, 10)
-	print(left_pad(tostr(game.score), 5, "0"), 42, 0, 7)
-	print("next", 46, 8, 7)
+	print("★", 2, 1, 10)
+	print(left_pad(tostr(scores[game.mode + 1][1].score), 5, "0"), 10, 1, 10)
+	print(left_pad(tostr(game.score), 5, "0"), 43, 1, 7)
+	print("next", 47, 9, 7)
 
-	print("gems", 46, 30, 7)
-	print(left_pad(tostr(game.gems), 4, "0"), 46, 36, 7)
+	print("gems", 47, 31, 7)
+	print(left_pad(tostr(game.gems), 4, "0"), 47, 37, 7)
 
-	print("lvl", 48, 45, 7)
-	print(left_pad(tostr(game.level + 1), 2, "0"), 50, 51, 7)
+	print("lvl", 49, 46, 7)
+	print(left_pad(tostr(game.level + 1), 2, "0"), 51, 52, 7)
 end
